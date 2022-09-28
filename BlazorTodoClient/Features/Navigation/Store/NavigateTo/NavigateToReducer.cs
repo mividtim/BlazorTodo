4a7a4c4a7173a1ceeaf592d77e1defb00d164d48
Tim Garthwaite <tim.garthwaite@uxmagicians.com>
2022-09-28 16:47:55 -0400

    Don't put authx routes into history

--- BlazorTodoClient/Features/Navigation/Store/NavigateTo/NavigateToReducer.cs
+++ BlazorTodoClient/Features/Navigation/Store/NavigateTo/NavigateToReducer.cs
@@ -7,9 +7,11 @@ namespace BlazorTodoClient.Features.Navigation.Store.NavigateTo;
 [SuppressMessage("ReSharper", "UnusedMember.Global")]
 public static class NavigateToReducer
 {
+    private static readonly string[] AuthxRoutes = new[] { "/login", "/register " };
+
     [ReducerMethod]
     public static NavigationState ReduceNavigateTo(NavigationState state, NavigateToAction action) =>
-        action.Route == state.CurrentRoute
+        action.Route == state.CurrentRoute || AuthxRoutes.Contains(action.Route)
             ? state
             : new NavigationState(action.Route, state.CurrentRoute);
 }
